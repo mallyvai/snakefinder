a8ec41f22536d44dfda8e7ce6a8bc116812c4eeb
virtualmachine <virtualmachine@virtualmachine-laptop.(none)>
2009-04-15 13:23:52 -0400

    almost there... finish up query parser.

--- query.py
+++ query.py
@@ -1,10 +1,11 @@
 from cPickle import load
 
-blocks = ["class", "def", "file"]
+q_blocks = ["class", "def", "file"]
 
-contains = "CONTAINS"
-peers = "OR"
-regex_name = "NAME"
+q_contains = "CONTAINS"
+q_peers = "OR"
+q_regex_name = "NAME"
+q_regex_name_val_sep = "="
 
 start_elts = blocks + regex_name
 
@@ -24,7 +25,7 @@ def enforce_in(elt, items):
 
 def get_matching_children(parent_url, matcher):
     matching = [i for i in index.children[parent_url] if matcher.match(i)]
-    return matching
+    return set(matching)
 
 def get_matching_ancestors(parent_url, matcher):
     investigated_children = []
@@ -37,25 +38,40 @@ def get_matching_ancestors(parent_url, matcher):
     
     #We've found all ancestors. Now let's go ahead and see if any of them match up.
     matching = [i for i in investigated_children if matcher.match(i)]
-    return matching
+    return set(matching)
 
-def handle_query(query, index):
+def handle_query_helper(query_part, index):
     graph = index.block_graph
-    parts = component.split()
+    parts = query_part.split()
+    
     start = parts[0]
-    pattern = parts[1]
+    raw_pattern = parts[0].split(q_regex_name_val_sep)[1]
+    flat_list = graph.classes #Small optimizations
 
-    flat_list = graph.classes #Small optimization
     if start == "class":
         pass #Instead of the standard None - why though? They both seem to have the same effect. Ask... 
     elif start == "def":
         flat_list = graph.defs
+        raw_pattern = parts[1].split(q_regex_name_val_sep)[1]
     elif start == "file":
         flat_list = graph.files
+        raw_pattern = parts[1].split(q_regex_name_val_sep)[1]
     elif start == regex_name:
         flat_list |= graph.defs | graph.files
     else:
         raise MalformedQueryException
+    
+    matcher = re.compile(raw_pattern)
+        
+    matching = set([i for i in for url in flat_list if matcher.match(i)])
+    return matching
+
+def handle_query(query):
+"""
+Sample queries: class=asdfj >
+"""
+
+
 
 if __name__ == "__main__":
     fh_input = open(index_file, 'wb')
