d30bbca7afe33732b2551f2596cfbd3b69760ced
Vaibhav Mallya <mallyvai@umich.edu>
2009-04-20 05:13:21 -0400

    it indexes using cool new methods for serializing the functools.partial thing needed it because im using a partioning scheme for the block-types now.

--- indexer.py
+++ indexer.py
@@ -4,8 +4,9 @@ import os, sys
 from os.path import join, abspath as abs
 import re
 from cPickle import dump
-
-from query_ds import URL, BlockGraph
+import functools
+from query_ds import URL, BlockGraph, Children_ToReduce, Children_FromReduce
+import copy_reg
 
 def line_indentation(line):
     """Get a line's left whitespace count."""
@@ -19,6 +20,7 @@ def iter_filenames(pathname, filename_matcher):
             if filename_matcher.match(filename):
                 yield full
 
+DBG_FILENAME = None
 class Indexer:
     def __init__(self, block_graph=None):
         if block_graph is None:
@@ -26,17 +28,23 @@ class Indexer:
                                     classes=set(),
                                     files=set(),
                                     parents=dict(),
-                                    children=defaultdict(set))
-
+                                    children=defaultdict(functools.partial(defaultdict, set)) )
+                                    #children=defaultdict( lambda:defaultdict(set) ))
+            # This last one is equal to defaultdict(functools.partial(defaultdict, set)) ()
+            # Note the extra brackets at the end. really need to learn more FP...
         self.block_graph = block_graph
+        copy_reg.pickle(functools.partial, Children_ToReduce)
 
-    def construct_helper(self, text, i, parent_url, parent_indentation):
+    def construct_helper(self, text, i, parent_url, parent_indentation, block_type):
+        global DBG_FILENAME
         """Recursively analyzes a file and generates the parent/child graph 
         and flat lists for functions and classes.
         """
+
         this_url, this_indent = URL(filename, i+1, text[i].strip()), line_indentation(text[i])
         self.block_graph.parents[this_url] = (parent_url)
-        self.block_graph.children[parent_url].add(this_url)
+        self.block_graph.children[parent_url][block_type].add(this_url)
+        
 
         while len(text) > i+1:
             i += 1
@@ -47,32 +55,42 @@ class Indexer:
                 return i
 
             line = text[i]
-            url = URL(filename, i+1, line.strip()) 
-            first = line.split()
+            tokens = line.split()
+            url = URL(filename, i+1, line.strip())
             
             if indent == parent_indentation:
                 purl, pindent = parent_url, parent_indentation
             elif indent > parent_indentation:
                 purl, pindent = this_url, indent
             
-            if len(first) > 0 and first[0] == "def":
+            # We now add the URL to its appropriate universal set
+            # based on its block type
+            block_type = None
+            if len(tokens) > 0:
+                block_type = tokens[0]
+            
+            if block_type == "def":
                 self.block_graph.defs.add(url)
-                i = self.construct_helper(text, i, purl, pindent)
+                i = self.construct_helper(text, i, purl, pindent, block_type)
         
-            elif len(first) > 0 and first[0] == "class":
+            elif block_type == "class":
                 self.block_graph.classes.add(url)
-                i = self.construct_helper(text, i, purl, pindent)
+                i = self.construct_helper(text, i, purl, pindent, block_type)
 
         return i
 
     def construct(self, filename):
         """ Sets up the call to construct_helper that generates the graph for this file."""
+        global DBG_FILENAME
+        DBG_FILENAME = filename
         file_url = URL(filename, -1, filename)
         fh = open(filename, 'r')
         lines = fh.readlines()
         fh.close()
 
-        self.construct_helper(lines, -1, file_url, 0)
+        #It's possible to have empty .py files (__init__.py) 
+        if len(lines) > 0:
+            self.construct_helper(lines, -1, file_url, 0, "file")
 
 
 if __name__ == "__main__":
@@ -91,5 +109,8 @@ if __name__ == "__main__":
     dump(index, fh_output)
     fh_output.close()
 
-    #pp = pprint.PrettyPrinter(indent=4)
-    #pp.pprint(index.block_graph.parents
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(index.block_graph.parents)
+
+# Thanks to http://mail.python.org/pipermail/python-list/2000-January/021385.html
+# For a wonderful little multi-dimensional dictionary hack that i never actually ended up using.
--- query_ds.py
+++ query_ds.py
@@ -1,4 +1,8 @@
+from functools import partial
+from pickletools import dis
+from pickle import loads, dumps
 from collections import namedtuple
+from copy_reg import pickle
 
 class MalformedQueryException(Exception):
     def __init__(self, value):
@@ -16,3 +20,22 @@ URL = namedtuple("URL", "file lineno statement")
 URLandType = namedtuple("URLandType", "url type")
 BlockGraph = namedtuple("BlockGraph", "defs classes files parents children")
 
+block_types = set(["file", "class", "def"])
+
+
+# I did *not* write this code; it is courtesy of nabit-hab http://codepad.org/BSYSEJC7/fork
+import functools, collections, pickle, pickletools, copy_reg
+
+def Children_FromReduce(obj_type, func, args, kwargs):
+    return obj_type(func, *args, **kwargs)
+
+def Children_ToReduce(obj):
+    return (Children_FromReduce, (type(obj), obj.func, obj.args, obj.keywords or {}))
+
+"""pickle(partial, to_reduce)
+
+dd = defaultdict(partial(defaultdict, set))
+s = dumps(dd)
+print s
+dis(s)
+print loads(s)"""
