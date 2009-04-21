402a792e756b8f3387c7f8edab0f878f0273c69d
Vaibhav Mallya <mallyvai@umich.edu>
2009-04-20 22:24:24 -0400

    cleaner query handler.

--- new_query.py
+++ new_query.py
@@ -1,102 +1,112 @@
-from query_exceptions import MalformedQueryException, NoTypeException
-from query_ds import URL, URLandType
+from query_ds import URL, URLandType, MalformedQueryException, InvalidBlockTypeException, block_types
+import re
+import itertools
 sep_parts = ","
 sep_st = "="
 
-def get_matching_children(part, index, parent_url):
-    type, matcher = get_type_and_matcher(part)
-    parent_index = index.children[parent_url]
-    results = set()
+class Query:
+    def _get_matching_children(self, part, parent_url):
+        block_type, matcher = get_type_and_matcher(part)
+        parent_index = self.index.children[parent_url]
+        results = set()
 
-    check = lambda a, b: True
-    if type != "all":
-        check = lambda uat, t: uat.type == t
+        check = lambda a, b: True
+        if block_type != "all":
+            check = lambda uat, t: uat.type == t
 
-    results = set()  
-    
-    if type == "all":
-        for partition_type, children in parent_index.iteritems():
-            for child in children:
-                if matcher.match(child):
-                    uat = URLandType(child, partition_type)
+        results = set()  
+        
+        if block_type == "all":
+            for partition_type, children in parent_index.iteritems():
+                for child in children:
+                    if matcher.match(child):
+                        uat = URLandType(child, partition_type)
+                        results += uat
+            return results
+
+        else:
+            for child in parent_index[block_type]:
+                if matchter.match(child):
+                    uat = URLandType(child, block_type)
                     results += uat
+            return results
+
+
+    def _get_matching_ancestors(self, part, parent_url):
+        block_type, matcher = get_type_and_matcher(part)
+        parent_index = self.index.children[parent_url]
+        uninvestigated_children = []
+
+        check = lambda a,b: True #Assume it's "all" initially. It will always be pass.
+        if block_type != "all":
+            check = lambda uat, t: uat.type == t #Return true if the URLandType has an equal type to the one passed in
+
+        results = set()
+        while True:
+            next_batch = []
+            for partition_type, children in parent_index.iteritems():
+                for child in children:
+                    uat = URLandType(child, partition_type)
+                    next_batch.append(uat)
+            uninvestigated_children.extend(next_batch)
+            if len(uninvestigated_children) is 0: #Leave; no more stuff to look at.
+                break
+            #Get the next one in line and its dictionary.
+            cur_uat = uninvestigated_children.pop()
+            parent_index = self.index.children[cur_uat.url]
+            #If it's the type we're looking for, and the regex matches the source snippet, it works.
+            if check(cur_uat, block_type) and matcher.match(cur_uat.url.statement):
+                results += cur_uat
         return results
 
-    else:
-        for child in parent_index[type]:
-            if matchter.match(child):
-                uat = URLandType(child, type)
-                results += uat
-        return results
 
-
-def get_matching_ancestors(part, index, parent_url):
-    type, matcher = get_type_and_matcher(part)
-    parent_index = index.children[parent_url]
-    uninvestigated_children = []
-
-    check = lambda a,b: True #Assume it's "all" initially. It will always be pass.
-    if type != "all":
-        check = lambda uat, t: uat.type == t #Return true if the URLandType has an equal type to the one passed in
-
-    results = set()
-    while True:
-        next_batch = []
-        for partition_type, children in parent_index.iteritems():
-            for child in children:
-                uat = URLandType(child, partition_type)
-                next_batch.append(uat)
-        uninvestigated_children.extend(next_batch)
-        if len(uninvestigated_children) is 0: #Leave; no more stuff to look at.
-            break
-        #Get the next one in line and its dictionary.
-        cur_uat = uninvestigated_children.pop()
-        parent_index = index.children[cur_uat.url]
-        #If it's the type we're looking for, and the regex matches the source snippet, it works.
-        if check(cur_uat, type) and matcher.match(cur_uat.url.statement):
-            results += cur_uat
-    return results
-
-#memoize this thing!
-def get_type_and_matcher(part):
-    split = part.split(sep_st)
-    if len(split is 1):
-        return "all", re.compile(part)
-    elif len(split) != 2:
-        raise MalformedQueryException(part)
-    else:
-        if split[0] not in block_types:
+    #memoize this thing!
+    def _get_type_and_matcher(self, part):
+        split = part.split(sep_st)
+        if len(split is 1):
+            return "all", re.compile(part)
+        elif len(split) != 2:
             raise MalformedQueryException(part)
-        return split[0], re.compile(split[1])
+        else:
+            if split[0] not in block_types:
+                raise MalformedQueryException(part)
+            return split[0], re.compile(split[1])
+    
+    def _iter_components(self, query):
+        re.findall("\s+>\s+.+|\s+>>\s+.+", query)
 
-def get_parts(component):
-    return component.split(sep_parts)
-def get_components(query):
-    return re.split(query, ">|>>")
+    def __init__(self, index):
+        self.index = index
+        self.universal_set = itertools.chain(index.files, index.defs, index.classes)
+        self.universal_parent = 
 
-def query_handler(query)
-    components = get_components(query)
-    start_component = components.pop(0)
-    start_parts = get_parts(start_component)
+    def _unify_sets(self):
+        """
+        A cheap way to fake a universal set of all URLs.
+        """
+        for i in itertools.chain(
+    
 
-    result_set = set()
-    for part in parts:
-        for url in index.universal_sets:
-            result_set += get_matching_descendants(part, index, url) 
-
-    result_set = index.universal_sets
-    for component in components:
-        descendant_result_set = set()
-        if depth is ">>":
-            for result in result_set:
-                for part in component:
-                    descendant_result_set += get_matching_descendants(result, part, index)
-        elif depth is ">":
-            for result in result_set:
-                for part in component:
-                    descendant_result_set += get_matching_children(result, part, index)
-        result_set = descendant_result_set
-    return result_set   
+    def handle_query(self, query):
+        result_set = set()
+        first = True
+        for depth, component in self._iter_components(query):
+            if first:
+                first = False
+                for part in self._iter_parts(component):
+                    result_set += _get_matching_descendants(part, self.universal_parent)
+            else:
+                for part in self._iter_parts(component):
+                    next_result_set = set()
+                    if depth is ">>":
+                        for url in result_set:
+                            next_result_set += self._get_matching_descendants(url, part)
+                    elif depth is ">":
+                        for url in result_set:
+                            next_result_set += self._get_matching_children(url, part)
+                    result_set = next_result_set
+        
+        return result_set   
 
 query = ". >> ."
 
