f9fcfa7cec7f1678444b7dc8faeb34f01b0ac68e
virtualmachine <virtualmachine@virtualmachine-laptop.(none)>
2009-05-11 21:58:23 -0400

    documenting the lexer. Also, returning the indent.

--- new_lexer.py
+++ new_lexer.py
@@ -5,7 +5,20 @@ import collections
 RowCol = collections.namedtuple("RowCol", "row col")
 NamedToken  = collections.namedtuple("NamedToken", "type string start end line")
 
+Line = collections.namedtuple("Line", "string indent")
+
 def generate_named_tokens(filename):
+    """
+    Accepts a python script's filename
+
+    Wraps tokenize.generate_tokens to
+    return the token, but in namedtuple
+    form:
+    (type, string, start, end, line).
+
+    where start and end each are
+    (row, col)
+    """
     fh = open(filename)
     for t in tokenize.generate_tokens(fh.readline):
         next_token = NamedToken(type = t[0],
@@ -17,28 +30,43 @@ def generate_named_tokens(filename):
     fh.close()
 
 def generate_logical_lines(filename):
-    line = "" 
+    """
+    Accepts a python script's filename
+    
+    Yields the tuple
+    (line, indent)
+    for each class or function declaration
+    in the script.
+
+    Lines are "logical lines". 
+    All whitespaces, including newlines,
+    are removed. F.ex:
+
+    def func(a = 222,
+            b, d)
+
+    will be yielded as
+
+    (func(a=222,b,d), 0)
+    """
     line_started = False
+    
     for tok in generate_named_tokens(filename):
-        #print tok.type, tok.string
-        if not line_started and tok.type == tokenize.NAME and tok.string == "class":
-            line_started = True
-            line = ""
-        elif not line_started and tok.type == tokenize.NAME and tok.string == "def":
-            line_started = True
-            line = ""
 
-        #If a logical newline; we're done with the definition
-        elif line_started and tok.type == tokenize.NEWLINE or tok.type == tokenize.ENDMARKER and len(line) > 0:
-            line_started = False
-            yield line
-            line = ""
+        # Flag it if the token's a new class or def declaration
+        if not line_started and tok.type == tokenize.NAME:
+            if tok.string == "class" or tok.string == "def":
+                line_started = True
+                line, indent = "", tok.start.col
+        
+        elif line_started:
+        # If a logical newline, we're done with the definition
+            if tok.type == tokenize.NEWLINE or tok.type == tokenize.ENDMARKER:
+                if len(line) > 0:
+                    yield Line(string=line, indent=indent)
+                line_started = False
 
-        # Otherwise we're in the middle of a line, so we 
-        # need to append the token
-        elif line_started and not tok.string.isspace():
-            line += tok.string
-i = 0
-for line in generate_logical_lines("small_class.py"):
-    i+=1
-    print i, "logical line", line
+            # Otherwise we're in the middle of a line, so we 
+            # need to append the next non-empty token
+            elif not tok.string.isspace():
+                line += tok.string
